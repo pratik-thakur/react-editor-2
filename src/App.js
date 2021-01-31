2e7b8eae44fd540d3eb00a7ebbc17c32da90a261
pratik-thakur <pratikthakur421@gmail.com>
2021-01-31 14:15:49 +0530

    init commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
+import React,{useState} from 'react';
 import './App.css';
 
 function App() {
+ const string = []
+  const[value,setvalue]=useState('')
+
+  const handleInputchange = (e)=>{
+   // const htmltag = document.querySelector('htmltag')
+    //htmltag.textContent=e.target.value
+    setvalue(e.target.value)
+  }
+
+  const addnew = (e)=>{
+    string.push("<div>"+value+"</div>")
+    console.log("<div>"+value+"</div>")
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div><h1 contentEditable="true">Enter title here </h1>
+      <h3 contentEditable="true">this is small description</h3>
+      <div className="content" contentEditable="true">
+        this or enteris content
+      </div>
+      </div>
+
+      <button onClick={addnew}>Add</button>
+      <textarea type="text" onChange={handleInputchange}></textarea>
+      <label id="htmltag">{value}</label>
     </div>
+    
   );
 }
 
